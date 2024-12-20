4354a4c779757a0578b76d6b4f3544c01c065ddb
lmfreire <l.matheus.freire10@gmail.com>
2024-12-20 12:39:59 -0300

    alteração no metodo de produtos para usar query

---
+++ src/material/grade.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('grade')
+export class Grade {
+    
+    @PrimaryColumn({name: 'cd_similar'})
+    cd_mat: string;
+
+    @Column()
+    nm_mat: string;
+
+    @Column({name: 'un_prod'})
+    nm_unid: string;
+    
+    @Column()
+    cd_gtin: string;
+    
+    @Column()
+    nr_ref: string;
+    
+    @Column()
+    vl_unit: string;
+    
+    @Column()
+    vl_prazo: string;
+
+    @Column()
+    tp_mat: string;
+
+    @Column()
+    sn_fora: string;
+
+    @Column()
+    tp_item: string;
+}
--- src/material/material.service.ts
+++ src/material/material.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DataSource, Not, Repository } from 'typeorm';
-import { Material } from './repres.entity';
+import { Material } from './material.entity';
 import getTenantRepository from 'src/utils/get.tenant.repository';
 
 @Injectable()
@@ -19,19 +19,45 @@ export class MaterialService {
     async getProdutos(tentat: string){
         const repository: Repository<Material> = await getTenantRepository(tentat, Material, this.getTenantDataSource);
 
-        return await repository.find({
-            where: [
-                {
-                  tp_mat: Not('3'),
-                  sn_fora: '0',
-                  tp_item: '00',
-                },
-                {
-                  tp_mat: Not('3'),
-                  sn_fora: '0',
-                  tp_item: '04',
-                },
-              ],
-        })
+        return await repository.query(`
+            SELECT
+              material.cd_mat,
+              material.nm_mat,
+              material.nm_unid,
+              material.nr_ref,
+              material.vl_unit,
+              material.vl_prazo,
+              material.tp_mat,
+              material.sn_fora,
+              material.tp_item,
+              material.cd_gtin
+          FROM
+              material
+          WHERE
+              tp_mat != '3' 
+              AND material.sn_fora = '0'
+              AND( material.tp_item = '00' OR material.tp_item = '04')
+
+          UNION ALL
+
+          SELECT
+              grade.cd_similar AS cd_mat,
+              material.nm_mat,
+              grade.un_prod AS nm_unid,
+              material.nr_ref,
+              grade.vl_unit,
+              grade.vl_prazo,
+              material.tp_mat,
+              material.sn_fora,
+              material.tp_item,
+              grade.cd_gtin
+          FROM
+              grade
+          LEFT JOIN material ON grade.cd_mat = material.cd_mat
+          WHERE
+              tp_mat != '3' 
+              AND material.sn_fora = '0'
+              AND( material.tp_item = '00' OR material.tp_item = '04') 
+          `)      
     }
 }
