4f54766b755083a2dde9737d393303b56a420225
lmFreire <l.matheus.freire10@gmail.com>
2025-01-09 21:59:55 -0300

    finalizando getMateriaisComposicao

--- src/materialsac/materialsac.controller.ts
+++ src/materialsac/materialsac.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { MaterialsacService } from './materialsac.service';
+import { Response } from 'express';
+import { ErrorResponse } from '../utils/error.resource';
 
 @Controller('materialsac')
 export class MaterialsacController {
@@ -9,9 +11,22 @@ export class MaterialsacController {
     ){}
 
     @Post('materiais_composicao')
-    async getMateriaisComposicao(@Req() req: Request, @Body() data: {cd_mat: string, cd_fil: string, cd_arm: string}){
+    async getMateriaisComposicao(
+        @Req() req: Request,
+        @Res() res: Response, 
+        @Body() data: {cd_mat: string, cd_fil: string, cd_arm: string}
+    ){
         const tenantId = req['tenantId'];
         
-        return await this.materialsacService.materiaisComposicao(tenantId, data);
+        //return await this.materialsacService.materiaisComposicao(tenantId, data);
+
+        const result = await this.materialsacService.materiaisComposicao(tenantId, data);
+
+        if(result.isError()){
+            const error = result.error
+            return res.status(error.status).send(new ErrorResponse(error.message));
+        }
+
+        return res.status(HttpStatus.ACCEPTED).send(result.value);
     }
 }
--- src/materialsac/materialsac.service.ts
+++ src/materialsac/materialsac.service.ts
@@ -1,7 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { Materialsac } from './materialsac.entity';
 import getTenantRepository from '../utils/get.tenant.repository';
+import { Estoque } from '../estoque/estoque.entity';
+import { Result } from '../utils/result';
+import { HttpError } from '../utils/httpError';
 
 @Injectable()
 export class MaterialsacService {
@@ -14,12 +17,32 @@ export class MaterialsacService {
     async materiaisComposicao(tentat: string, data: {cd_mat: string, cd_fil: string, cd_arm: string}){
         const repository: Repository<Materialsac> = await getTenantRepository(tentat, Materialsac, this.getTenantDataSource);
 
-        const res =  await repository.find({
+        const res: Materialsac[] =  await repository.find({
             where: {
                 cd_mat: data.cd_mat
             }
         });
 
-        return res
+        res.forEach(async (item : Materialsac) => {
+            const repository: Repository<Estoque> = await getTenantRepository(tentat, Estoque, this.getTenantDataSource);
+            
+            const estoque: Estoque = await repository.findOne({
+                where: {
+                    cd_mat: item.cd_sac,
+                    cd_fil: data.cd_fil,
+                    cd_arm: data.cd_arm
+                }
+            })
+            
+            if(estoque && estoque.sn_inventario == '1'){
+                return new Result(null, 
+                    new HttpError(`Produto de Composição: ${item.cd_sac} Bloqueado - Inventário de Estoque`, HttpStatus.CONFLICT)
+                )
+            }
+        })
+
+        
+
+        return new Result(res,null)
     }
 }
